5c79474524a842b1e80135c64caa24859e5f685e
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-22 19:08:46 +0300

    Создание класса LogFormatter, который преобразует LogEntry в строку выходного формата.

---
+++ LogStandardizer.Core/Models/LogFormatter.cs
@@ -0,0 +1,18 @@
+﻿namespace LogStandardizer.Core.Models;
+
+public static class LogFormatter
+{
+    public static string Format(LogEntry entry)
+    {
+        string dateStr = entry.Date.ToString("dd-MM-yyyy");
+
+        return string.Join(Environment.NewLine,
+        [
+            dateStr,
+            entry.Time,
+            entry.LogLevel,
+            entry.CallerMethod,
+            entry.Message
+        ]);
+    }
+}
